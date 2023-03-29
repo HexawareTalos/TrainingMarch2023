ad8d575875538442924c97b22dc325afca2c5e41
tony <tonyw@hexaware.com>
2023-03-29 08:02:30 +0100

    Added new Feature file Removed Runwith annotation in Runner.java

--- src/test/java/automation/training/Runner.java
+++ src/test/java/automation/training/Runner.java
@@ -4,7 +4,7 @@ package automation.training;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-//@RunWith(Cucumber.class)
+
 @CucumberOptions(features = "src/test/java/features",
         glue = "automation.training",
         publish = true,
